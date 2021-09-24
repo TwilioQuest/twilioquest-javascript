5f894e7f9efa49b4cb77616f644d31cd8bf836e2
Ryan Kubik <rkubik@twilio.com>
2021-09-24 14:42:29 -0700

    Add lasers and ducktypium objects

---
+++ objects/ducktypium-container/config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  animations: {},
+  spriteSheets: {
+    twilioquestDucktypiumContainer: {
+      fileName: "WaterTank.png",
+      frameDimensions: {
+        width: 24,
+        height: 72,
+      },
+    },
+  },
+  properties: {
+    sprite: {
+      defaultFrameIndex: 10,
+      spriteSheet: "twilioquestDucktypiumContainer",
+      layers: [],
+    },
+  },
+};
