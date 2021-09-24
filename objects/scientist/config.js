603b01d3f9a13a4a9e68dde28d00cc9307efefe7
Ryan Kubik <rkubik@twilio.com>
2021-09-24 10:57:32 -0700

    Create all scientists

--- objects/scientist/config.js
+++ objects/scientist/config.js
@@ -1,6 +1,24 @@
 const conversationOnInteract = require("../../scripts/conversationOnInteract");
 
 module.exports = {
+  animations: {
+    scientist1_idle: {
+      frames: [0, 1, 2, 3],
+      frameRate: 4,
+    },
+    scientist2_idle: {
+      frames: [6, 7, 8, 9],
+      frameRate: 4,
+    },
+    scientist3_idle: {
+      frames: [12, 13, 14, 15],
+      frameRate: 4,
+    },
+    scientist4_idle: {
+      frames: [18, 19, 20, 21],
+      frameRate: 4,
+    },
+  },
   spriteSheets: {
     twilioQuestJavaScriptScientists: {
       fileName: "NPC_Scientists.png",
@@ -11,6 +29,24 @@ module.exports = {
     },
   },
   events: {
+    onMapDidLoad: (self, event, world) => {
+      const scientistFrames = {
+        scientist1: 0,
+        scientist2: 6,
+        scientist3: 12,
+        scientist4: 18,
+      };
+
+      self.sprite.frame = scientistFrames[self.scientist];
+
+      self.createIdleAnimations({
+        animations: {
+          [`${self.scientist}_idle`]: 100,
+        },
+        minIdleTime: 2000,
+        maxIdleTime: 5000,
+      });
+    },
     onPlayerDidInteract: conversationOnInteract,
   },
   properties: {
