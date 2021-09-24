1b389b6cff104d89d28d633fac99a7c44e97ea41
Ryan Kubik <rkubik@twilio.com>
2021-09-24 15:35:44 -0700

    Add password scripting

--- levels/javascript/events.js
+++ levels/javascript/events.js
@@ -1,16 +1,28 @@
 const handleGeniusBar = require("./events/handleGeniusBar");
 const handleDoorControls = require("./events/handleDoorControls");
 const { WORLD_STATE_KEY } = require("../../scripts/config");
+const merge = require("lodash.merge");
 
 const INITIAL_STATE = {
   accessLevels: {},
   decontamination: {
     areaEntered: "",
   },
+  room1: {
+    passwordFound: false,
+    explosionTriggered: false,
+    jsEnableLaserFinished: false,
+  },
 };
 
 module.exports = function (event, world) {
-  const worldState = world.getState(WORLD_STATE_KEY) || INITIAL_STATE;
+  const worldState = merge(INITIAL_STATE, world.getState(WORLD_STATE_KEY));
+
+  if (event.name === "playerDidInteract") {
+    if (event.target.stasisBeamPassword) {
+      worldState.room1.passwordFound = true;
+    }
+  }
 
   // Handle first run experience aboard Fog Owl
   handleGeniusBar(event, world, worldState);
